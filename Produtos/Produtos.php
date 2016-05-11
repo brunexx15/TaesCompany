33786e27746861f85ed0cbf45476f18a7b180910
Brunoxx15 <brunoxx15@gmail.com>
2016-05-11 11:21:27 -0400

    Atulizações gerais

---
+++ Produtos/Produtos.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>Produtos</h1>
+        <h2>Em desenvolvimento!!!</h2>
+        <br/>
+        Estamos desenvolvendo um jogo voltado para pessoas que querem aprender oque é programar, um jogo<br/>
+        indicado para pessoas que não tem nenhum conhecimento na area da programação.<br/>
+        Um jogo básico, em terceira pessoa que, basicamente tera que resolver alguns problemas lógicos para subir de nível,<br/>
+        este jogo esta sendo desenvolvido no UDK (Unreal Development Kit). 
+        <br/>
+        <br/>
+        <img src="UDK.jpg" alt="" width="180" height="120" /> 
+    </body>
+</html>
--- index.php
+++ index.php
@@ -6,23 +6,29 @@
     </head>
     <body>
         <h1>TaesCompany</h1>
-        <form>
-            <img src=""
-        </form>
-        <form>
-            Produtos:Nada
-        </form>
-        <form>
-            Parceiros:Ninguem
-        </form>
-        <form>
-            Tecnlogia:Não sei
-        </form>
-        <form>
-            <pre>Area de Contato:
-            Email:
-            Telefone:
-            </pre>
-        </form>
+        
+            <img src="logo.png" alt="">
+        <br/>
+        
+        <h2>Confira nossos produtos</h2>
+                <a href="Produtos/Produtos.php">clicando aqui</a> 
+        
+        
+        <br/>
+        <br/>
+            Produtos: Nada
+        <br/>
+            Parceiros: 0 variavel inconstante 
+        <br/>
+            Tecnlogia: Não sei
+        <br/>
+            
+            <h2>Entre em contato conosco:</h2>
+            
+            Email: taescompany@gmail.com
+            <br/>
+            Telefone: (xx)xxxx-xxxx
+          
+        
     </body>
 </html>
