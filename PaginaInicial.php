9b6f081006d6b45f12c140fd536ab3ccf917a73f
Brunoxx15 <brunoxx15@gmail.com>
2016-05-11 11:28:35 -0400

    Mais Auterações!!!

---
+++ PaginaInicial.php
@@ -0,0 +1,32 @@
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>TaesCompany</title>
+    </head>
+    <body>
+        <h1>TaesCompany</h1>
+        
+            <img src="logo.png" alt="">
+        <br/>
+        
+        <h2>Confira nossos produtos</h2>
+                <a href="Produtos/Produtos.php">clicando aqui</a> 
+        
+        
+        <br/>
+        <br/>
+            Produtos: Em desenvolvimento
+        <br/>
+            Parceiros: 0 variavel inconstante 
+        <br/>
+            
+            <h2>Entre em contato conosco:</h2>
+            
+            Email: taescompany@gmail.com
+            <br/>
+            Telefone: (xx)xxxx-xxxx
+          
+        
+    </body>
+</html>
